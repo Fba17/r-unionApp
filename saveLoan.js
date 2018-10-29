25471fa692fd5c1ff172c958eda5b7610ae9fc20
boris Fosso <bfossongounou@deloitte.de>
2018-10-29 16:49:08 +0100

    change id in investissementsOrg.json to be int implement action to change the investment amount of each investor

--- saveLoan.js
+++ saveLoan.js
@@ -1,9 +1,6 @@
 var requestURL = 'https://africorp.github.io/appreunion/json/investissementsOrg.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
-request.responseType = 'json';var requestURL = 'https://africorp.github.io/appreunion/json/investissementsOrg.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
@@ -25,34 +22,83 @@ request.onload = function() {
       investisseurs: lisInvestment.filter(x => x.date === currentDate).map(x => 
        {
         var membre = {};
-        membre["investisseur"] = x.investisseur;
+        membre["idInvestisseur"] = x.id;
         membre["montant"] = x.montant;
         return membre;
        })
     })
   }
+  
   var guiInput = {
     prêteur: "Boris" ,
-    date:"2018.03.24" ,
-    montant: 1000
+    date:"2018.03.27" ,
+    montant: 5000,
+    id: 1
   }
   var datePret = new Date(guiInput.date);
   var montantPret = guiInput.montant;
   var gpr = 0
   var consideredGrp = listGrpInvestment.filter(x => x.date <= datePret)
-  while(montantPret > 0 && consideredGrp[gpr]) {
+  var listLoanInvestor = []
+  var montantPretVar = montantPret 
+
+  while(montantPretVar > 0 && consideredGrp[gpr]) {
     var actGrpe = consideredGrp[gpr]
     var montantGrpe = actGrpe.montant;
-    if(montantGrpe >= montantPret)
+    if(montantGrpe >= montantPretVar)
     {
-      montantPret = 0;
-      montantGrpe = montantGrpe - montantPret
+      var percentage = montantPretVar / montantPret;
+      listLoanInvestor.push({
+        id:actGrpe.id,
+        montantEmprunte:montantPretVar, 
+        percentage: percentage
+      })
+      montantPretVar = 0;
+      montantGrpe = montantGrpe - montantPretVar;
     }
     else{
-    	montantPret = montantPret - montantGrpe  
+        var percentage = montantGrpe / montantPret;
+        listLoanInvestor.push({
+          id:actGrpe.id,
+          montantEmprunte: montantGrpe, 
+          percentage: percentage
+        })
+    	montantPretVar = montantPretVar - montantGrpe;
+        montantGrpe = 0;
+    }
+    gpr = gpr +1;
+  }
+  var pretObj = {
+    id:guiInput.id,
+    date:guiInput.date,
+    montant:guiInput.montant,
+    listOfInvestor:listLoanInvestor
+  }
+  console.log(lisInvestment)
+  console.log(listInvestisseurPerDate)
+  console.log(pretObj)
+  // change les investissement.
+  
+  var listOfInvestor = pretObj.listOfInvestor
+  var mutedInvestorsList = []
+  for (let investor of listOfInvestor){
+   var investors = listInvestisseurPerDate.filter(x => x.id == investor.id )
+   var investisseurLoan = investors.investisseurs
+   var ivestisseurIds = investisseurLoan.map(x => x.idInvestisseur)
+   var montantAretirer = investor.montantEmprunte / investisseurLoan.length
+    for (let invest of lisInvestment){
+      if (ivestisseurIds.includes(invest.id)){
+        mutedInvestorsList.push({
+          id:invest.id,
+          investisseur: invest.investisseur,
+          date: invest.date,
+          montant: invest.montant - montantAretirer 
+        })
+      }
+      else{
+        mutedInvestorsList.push(invest)
+      }
     }
   }
-  console.log(new Date(guiInput.date))
-  console.log(listGrpInvestment)
-  console.log(listInvestisseurPerDate)
+    
 }
